6e85308bc7427c107270e361492ef8dafafb2517
Sharan Velusamy <sharan.velusamy@pfizer.com>
2026-04-01 23:35:22 +0530

    - Updated skeleton loader for the menu and category manage page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,10 +2,8 @@
 
 import {MenuItem as TypeMenuItem} from "@/types/menu";
 import MenuAdd from "@/components/MenuAdd";
-import MenuItem from "@/components/MenuItem";
 import {useEffect, useState} from "react";
 import {ApiListResponse as TypeApiListResponse} from "@/types/global";
-import Loader from "@/components/Loader";
 import {CategoryWithMenuItem as TypeCategoryWithMenuItem} from "@/types/category";
 import CategoryMenuItem from "@/components/CategoryMenuItem";
 
@@ -36,12 +34,6 @@ export default function MenuPage() {
     getMenuItems().then(r => console.log(r));
   }, []);
 
-  if (isPageLoading) {
-    return (
-      <Loader/>
-    );
-  }
-
   return (
     <div className="min-h-screen py-20 pt-32 bg-white dark:bg-gray-900">
       <MenuAdd
@@ -56,22 +48,52 @@ export default function MenuPage() {
           Manage Menu Item
         </h1>
 
-        <div className="flex flex-col gap-4">
-          {categoryWithMenuItems.map((categoryWithMenuItem: TypeCategoryWithMenuItem) => (
-            <CategoryMenuItem
-              key={categoryWithMenuItem.id}
-              categoryWithMenuItem={categoryWithMenuItem}
-              menuItemEditAction={(menu: TypeMenuItem) => {
-                setEditingMenu(menu);
-                window.scrollTo(0, 0)
-              }}
-              refreshMenuItems={() => {
-                getMenuItems().then(r => console.log(r));
-                setEditingMenu(null)
-              }}
-            />
-          ))}
-        </div>
+        {isPageLoading ? (
+          <div className="flex flex-col gap-4">
+            {Array.from({length: 3}).map((_, i) => (
+              <div key={i} className="bg-gray-50 dark:bg-gray-900 border-2 border-solid border-[#f0e6dd] dark:border-gray-700 rounded-2xl animate-pulse">
+                {/* Category header skeleton */}
+                <div className="flex items-center justify-between px-6 py-4">
+                  <div className="h-6 w-36 bg-gray-200 dark:bg-gray-700 rounded"></div>
+                  <div className="h-8 w-8 bg-gray-200 dark:bg-gray-700 rounded"></div>
+                </div>
+                {/* Menu item cards skeleton */}
+                <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3 px-5 pb-5">
+                  {Array.from({length: 3}).map((_, j) => (
+                    <div key={j} className="shadow-lg border-2 border-solid border-[#f0e6dd] dark:border-gray-600 rounded-2xl p-4.5 bg-[#fffbf6] dark:bg-gray-950 flex justify-between items-center">
+                      <div className="flex flex-col gap-2 flex-1">
+                        <div className="h-5 w-28 bg-gray-200 dark:bg-gray-700 rounded"></div>
+                        <div className="h-3.5 w-40 bg-gray-200 dark:bg-gray-700 rounded"></div>
+                        <div className="h-4 w-16 bg-gray-200 dark:bg-gray-700 rounded"></div>
+                      </div>
+                      <div className="flex gap-4">
+                        <div className="h-10 w-16 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                        <div className="h-10 w-20 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-col gap-4">
+            {categoryWithMenuItems.map((categoryWithMenuItem: TypeCategoryWithMenuItem) => (
+              <CategoryMenuItem
+                key={categoryWithMenuItem.id}
+                categoryWithMenuItem={categoryWithMenuItem}
+                menuItemEditAction={(menu: TypeMenuItem) => {
+                  setEditingMenu(menu);
+                  window.scrollTo(0, 0)
+                }}
+                refreshMenuItems={() => {
+                  getMenuItems().then(r => console.log(r));
+                  setEditingMenu(null)
+                }}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
